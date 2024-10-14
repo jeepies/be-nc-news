febd7b0dff8d7cb5c2694b51290ba976d1c267b6
jay <davisjayalan@gmail.com>
2024-10-14 11:41:35 +0100

    added test for /api/articles/:id

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -39,3 +39,37 @@ describe("GET: /api/topics", () => {
       });
   });
 });
+
+describe("GET /api/articles/:id", () => {
+  it("should respond with an article object", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.author).not.toBe(undefined);
+        expect(body.title).not.toBe(undefined);
+        expect(body.article_id).not.toBe(undefined);
+        expect(body.body).not.toBe(undefined);
+        expect(body.topic).not.toBe(undefined);
+        expect(body.created_at).not.toBe(undefined);
+        expect(body.votes).not.toBe(undefined);
+        expect(body.article_img_url).not.toBe(undefined);
+      });
+  });
+  it("should return appropriately when given a valid ID that does not exist", () => {
+    return request(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toEqual("ID does not exist")
+      });
+  });
+  it("should return invalid request when passed in erroneous data", () => {
+    return request(app)
+      .get("/api/articles/id")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toEqual("Bad Request")
+      });
+  });
+});
