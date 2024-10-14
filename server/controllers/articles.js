2f9853517a175ea502bc43d8759a16c9b3350896
jay <davisjayalan@gmail.com>
2024-10-14 14:03:30 +0100

    added GET /api/articles/:id/comments

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -22,3 +22,17 @@ exports.getAll = (request, response, next) => {
     })
     .catch((err) => next(err));
 };
+
+exports.getCommentsByID = (request, response, next) => {
+  const { id } = request.params;
+  model
+    .fetchCommentsByID(id)
+    .then((data) => {
+      if (data.length === 0)
+        response
+          .status(404)
+          .json({ message: "This article has no comments, or does not exist" });
+      else response.status(200).json({ comments: data });
+    })
+    .catch((err) => next(err));
+};
