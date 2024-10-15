fdd0f9d7b4d79391a3f18c49cdb237e9a99d2548
jay <davisjayalan@gmail.com>
2024-10-15 15:40:50 +0100

    added PATCH /api/comments/:id

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,7 +1,23 @@
 const db = require("../../db/connection");
 
+exports.getByID = (id) => {
+  return db
+    .query(`SELECT * FROM comments WHERE comment_id = $1`, [id])
+    .then((data) => data.rows[0]);
+};
+
 exports.deleteByID = (id) => {
-  return db.query("DELETE FROM comments WHERE comment_id = $1", [id]).then(
-    (data) => data.rowCount
-  );
+  return db
+    .query("DELETE FROM comments WHERE comment_id = $1", [id])
+    .then((data) => data.rowCount);
+};
+
+exports.update = (id, payload) => {
+  return db
+    .query(
+      `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *`,
+      [payload.inc_votes, id]
+    )
+    .then((data) => data.rows[0])
+    .catch(err => console.log(err));
 };
