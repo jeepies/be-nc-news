471b450fed7501fe0bfdca0293b2c554404d52f6
jay <davisjayalan@gmail.com>
2024-10-15 15:06:01 +0100

    added GET /api/users/:username

--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,7 @@ const controller = require("../server/controllers").users;
 
 const router = express.Router();
 
-router.get("/", controller.getAll)
+router.get("/", controller.getAll);
+router.get("/:username", controller.getByUsername);
 
 module.exports = router;
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,3 +6,18 @@ exports.getAll = (request, response, next) => {
     .then((data) => response.status(200).json({ users: data }))
     .catch((err) => next(err));
 };
+
+exports.getByUsername = (request, response, next) => {
+  const { username } = request.params;
+
+  // This is not required as params are always parsed as strings, and if a username of length
+  // of zero was passed, the route would be /api/users, which is a valid route. I suppose its
+  // the thought that counts?
+  // if (typeof username !== "string" || username.length === 0)
+  //   return response.status(400).json({ message: "Invalid username" });
+
+  model.fetchByUsername(username).then((data) => {
+    if (!data) return response.status(404).json({ message: "User not found" });
+    else return response.status(200).json(data);
+  });
+};
