229dd9553b999f860e4d9060702c3c9618f9a11a
jay <davisjayalan@gmail.com>
2024-10-15 11:43:13 +0100

    added comment_count to GET /api/articles/:id

--- server/models/articles.js
+++ server/models/articles.js
@@ -3,7 +3,11 @@ const format = require("pg-format");
 
 exports.fetchByID = (id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .query(`SELECT 
+        articles.article_id, articles.title, articles.author, 
+        articles.topic, articles.created_at, articles.votes,
+        articles.body, articles.article_img_url, COUNT(comments)::Int AS comment_count
+      FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`, [id])
     .then((data) => data);
 };
 
